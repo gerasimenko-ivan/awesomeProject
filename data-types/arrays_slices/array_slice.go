0dbe94b853e535e4c71cf69fdcddcfa061a567cb
ivgerasimenko <ivgerasimenko@ozon.ru>
2022-02-13 06:05:38 +0300

    array examples

---
+++ data-types/arrays_slices/array_slice.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+	// ARRAYS
+
+	var array0 = [2]string{} // length 2 / initialized by type zeros '' (e.g. empty string in this case)
+	printStrLen2ArrByPointer(&array0)
+
+	var array1 = [...]int{0, 1, 2, 3} // length 4, cause 4 elements / array cause [...]
+	printIntLen4ArrByValue(array1)    //fmt.Printf("arr: %v\n", array1)
+
+	// SLICES
+
+}
+
+// how to pass array to func - BY POINTER
+func printStrLen2ArrByPointer(arr *[2]string) {
+	fmt.Printf("arr: %v, length: %v\n", arr, len(arr))
+}
+
+// how to pass array to func - BY VALUE
+func printIntLen4ArrByValue(arr [4]int) {
+	fmt.Printf("arr: %v, length: %v\n", arr, len(arr))
+}
