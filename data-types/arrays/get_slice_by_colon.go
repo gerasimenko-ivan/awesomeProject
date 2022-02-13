200f2181c6f92ea8792478918e3affb31e8f3d6e
ivgerasimenko <ivgerasimenko@ozon.ru>
2022-02-13 05:36:23 +0300

    add func arrDoXBeforeColon()

--- data-types/arrays/get_slice_by_colon.go
+++ data-types/arrays/get_slice_by_colon.go
@@ -3,22 +3,26 @@ package main
 import "fmt"
 
 func main() {
-	// Need to know difference Cannot use '&x' (type *[6]int) as the type *[]int
-	//var x = [6]int{0, 1, 2, 3, 4, 5}
+	// Need to know difference:
+	//var x = [6]int{0, 1, 2, 3, 4, 5} // ARRAY - fixed length // or [...]int{0}
 	var x = []int{0, 1, 2, 3, 4, 5}
 
-	l := arrDoXAfterColon(&x, 2)
-	h := x[2:]
+	l := arrDoXAfterColon(&x, 2) // Cannot use '&x' (type *[6]int) as the type *[]int
+	h := arrDoXBeforeColon(&x, 2)
 	fmt.Printf("%v\n", l)
 	fmt.Printf("%v\n", h)
 	fmt.Println("hello world")
 }
 
-// how to pass array to func?
+// how to pass array/slice to func?
 //  - by pointer - V
 //  - by value - it is "expensive"
-func arrDoXAfterColon(a *[]int, x int) []int { // pass array by pointer
+func arrDoXAfterColon(a *[]int, x int) []int { // pass slice by pointer
 	// what is :2 or 2: in request to elements of array
 	// lets do it as func
 	return (*a)[:x] // returns slice up to X element, not including the X-element
+}
+
+func arrDoXBeforeColon(a *[]int, x int) []int { // pass slice by pointer
+	return (*a)[x:] // returns slice from the X-th element, including the X-element
 }
