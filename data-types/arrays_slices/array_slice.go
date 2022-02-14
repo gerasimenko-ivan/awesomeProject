d745b32f0c8d316aa5afa1eede1e9b914038cf68
ivgerasimenko <ivgerasimenko@ozon.ru>
2022-02-14 23:33:56 +0300

    example of iteration through array by range

--- data-types/arrays_slices/array_slice.go
+++ data-types/arrays_slices/array_slice.go
@@ -4,12 +4,18 @@ import "fmt"
 
 func main() {
 	// ARRAYS
+	// - fixed length
 
 	var array0 = [2]string{} // length 2 / initialized by type zeros '' (e.g. empty string in this case)
 	printStrLen2ArrByPointer(&array0)
 
 	var array1 = [...]int{0, 1, 2, 3} // length 4, cause 4 elements / array cause [...]
 	printIntLen4ArrByValue(array1)    //fmt.Printf("arr: %v\n", array1)
+
+	// iterate through
+	for i, el := range array0 {
+		fmt.Printf("i=%v el='%v'\n", i, el)
+	}
 
 	// SLICES
 
