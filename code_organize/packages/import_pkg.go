2ef6afe2a00b4c302e46d7f39f0eaeea483d4042
ivgerasimenko <ivgerasimenko@ozon.ru>
2022-02-19 23:18:38 +0300

    about factored and multiple import statements

--- code_organize/packages/import_pkg.go
+++ code_organize/packages/import_pkg.go
@@ -3,10 +3,14 @@ package main
 // Programs start running in package main.
 // So how do I have so many main packages??? :)
 
-import (
+import ( // "factored" import style (imports in parenthesis) - it's GOOD!
 	"fmt"
 	"math/rand"
 )
+
+// multiple import statements - BAD STYLE
+//import "fmt"
+//import "math/rand"
 
 func main() {
 	// the package (e.g. "rand") name is the same as the last element of the import path, by convention
